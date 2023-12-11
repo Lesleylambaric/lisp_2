881624c0c22018d405c0b679614ad284f17e0fab
Lesleylambaric <135842904+Lesleylambaric@users.noreply.github.com>
2023-12-10 19:41:00 -0500

    Update and rename lab.py to lispcode.py

--- lispcode.py
+++ lispcode.py
@@ -1,13 +1,11 @@
 """
-6.1010 Spring '23 Lab 12: LISP Interpreter Part 2
+LISP Interpreter 
 """
 #!/usr/bin/env python3
 import sys
 sys.setrecursionlimit(20_000)
 
-# KEEP THE ABOVE LINES INTACT, BUT REPLACE THIS COMMENT WITH YOUR lab.py FROM
-# THE PREVIOUS LAB, WHICH SHOULD BE THE STARTING POINT FOR THIS LAB.
-#!/usr/bin/env python3
+
 
 import sys
 import doctest
